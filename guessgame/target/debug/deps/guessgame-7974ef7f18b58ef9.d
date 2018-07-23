a8aadf6fcfc2090895ed17f803ad25c06893791f
Nguyen Dinh Phi <phind.uet@gmail.com>
2018-07-23 18:06:59 +0800

    getting input from the keyboard and then printing it.

---
+++ guessgame/target/debug/deps/guessgame-7974ef7f18b58ef9.d
@@ -0,0 +1,5 @@
+/home/phi/WS/RUST/Rust-in-one-week/guessgame/target/debug/deps/guessgame-7974ef7f18b58ef9: src/main.rs
+
+/home/phi/WS/RUST/Rust-in-one-week/guessgame/target/debug/deps/guessgame-7974ef7f18b58ef9.d: src/main.rs
+
+src/main.rs:
---
+++ guessgame/target/debug/guessgame.d
@@ -0,0 +1,1 @@
+/home/phi/WS/RUST/Rust-in-one-week/guessgame/target/debug/guessgame: /home/phi/WS/RUST/Rust-in-one-week/guessgame/src/main.rs
