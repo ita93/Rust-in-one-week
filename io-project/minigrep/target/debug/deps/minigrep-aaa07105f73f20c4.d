97712e948d1c10359e073cd002ce849d20baaea1
Nguyen Dinh Phi <phind.uet@gmail.com>
2018-12-27 19:11:58 +0800

    Print args value

---
+++ io-project/minigrep/target/debug/deps/minigrep-aaa07105f73f20c4.d
@@ -0,0 +1,5 @@
+/home/phi/WS/RUST/Rust-in-one-week/io-project/minigrep/target/debug/deps/minigrep-aaa07105f73f20c4: src/main.rs
+
+/home/phi/WS/RUST/Rust-in-one-week/io-project/minigrep/target/debug/deps/minigrep-aaa07105f73f20c4.d: src/main.rs
+
+src/main.rs:
---
+++ io-project/minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/home/phi/WS/RUST/Rust-in-one-week/io-project/minigrep/target/debug/minigrep: /home/phi/WS/RUST/Rust-in-one-week/io-project/minigrep/src/main.rs
