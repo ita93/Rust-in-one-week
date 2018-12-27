6850213bbd80ab54959c7ba55c8f69899ea154f8
Phi Nguyen <phind.uet@gmail.com>
2018-12-27 21:50:11 +0800

    Extract parsing to a function

--- io-project/minigrep/src/main.rs
+++ io-project/minigrep/src/main.rs
@@ -1,20 +1,27 @@
 use std::env; //For reading arguments
-use std::fs::File; 
-use std::io::prelude::*;
+//use std::fs::File; 
+//use std::io::prelude::*;
+use std::fs;
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
-  //the args[0] is binary file path, as same as C
-  let query = &args[1];
-  let filename = &args[2];
-  
-  println!("Searching for {}", query);
-  println!("In file {}", filename);
-  
-  //Reading file
-  let mut f = File::open(filename).expect("file not found");
-  let mut contents = String::new();
-  f.read_to_string(&mut contents).expect("Something went wrong reading the
-file");
-  println!("With text: \n{}", contents);
+    let args: Vec<String> = env::args().collect();
+    //the args[0] is binary file path, as same as C 
+    let config = parse_config(&args); 
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+    //Reading file
+    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+    println!("With text: \n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config {query, filename}
 }
