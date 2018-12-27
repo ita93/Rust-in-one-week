643fccdf4ace15d8b83b2c160d78ab150b5d1c27
Phi Nguyen <phind.uet@gmail.com>
2018-12-27 22:22:57 +0800

    - Extracting run function

--- io-project/minigrep/src/main.rs
+++ io-project/minigrep/src/main.rs
@@ -1,4 +1,6 @@
 use std::env; //For reading arguments
+use std::error::Error;
+use std::process;
 //use std::fs::File; 
 //use std::io::prelude::*;
 use std::fs;
@@ -6,12 +8,17 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     //the args[0] is binary file path, as same as C 
-    let config = parse_config(&args); 
+    let config = Config::new(&args).unwrap_or_else(|err| {
+      println!("Problem parsing arguments: {}", err);
+      process::exit(1);
+    });
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
     //Reading file
-    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-    println!("With text: \n{}", contents);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -19,9 +26,20 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        
+        Ok(Config {query, filename})
+    }
+}
 
-    Config {query, filename}
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    println!("With text: \n {}", contents);
+    Ok(())
 }
